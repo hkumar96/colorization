d3d661c1ae78ebee5cce2c1f7dbf12599f597cd6
Hemant Kumar <kumar96hemant@gmail.com>
2018-04-06 19:14:43 +0530

    first commit

---
+++ labrange.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = np.ones((256,256,3),dtype=np.uint8);
+A = np.reshape(np.repeat(np.arange(0,256,dtype=np.uint8),256,axis=0),(256,256));
+B = A.T;
+for i in range(256):
+	L = i*np.ones((256,256),dtype=np.uint8);
+	img[:,:,0] = L;
+	img[:,:,1] = A;
+	img[:,:,2] = B;
+	imgrgb = cv2.cvtColor(img,cv2.COLOR_LAB2RGB);
+	cv2.imwrite('labimages/'+str(i)+'.jpg',imgrgb)
---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+input_path  = "/home/hemantk/Documents/colorization/tiny-imagenet-200/train/"
+Q = 256
+
+def batchReadImages(dir_path):
+	abSpace = np.zeros((Q,Q));
+	numOfImg = 0;
+	for root,directories,filenames in os.walk(input_path):
+		if numOfImg > 1e6:
+			break;
+		for filename in filenames:
+			file_path = os.path.join(root,filename);
+			_,ext = os.path.splitext(file_path);
+			if not ext == '.txt':
+				numOfImg = numOfImg + 1;
+				# print(file_path)
+				print(numOfImg)
+				image = cv2.cvtColor(cv2.imread(file_path),cv2.COLOR_BGR2LAB);
+				tempa = np.floor(image[:,:,1].flatten().astype(float)).astype(np.uint8);
+				tempb = np.floor(image[:,:,2].flatten().astype(float)).astype(np.uint8);
+				# print(tempb,tempa)
+				abSpace[tempa,tempb] = abSpace[tempa,tempb] + 1;
+
+	return abSpace,numOfImg
+
+def mapSpace2Vec(wSpace):
+	return wSpace.flatten();
+
+def calcWeights(abSpace,numOfImg):
+	wSpace = 1/abSpace;
+	weights = mapSpace2Vec(wSpace);
+
+
+def main():
+	abSpace,numOfImg = batchReadImages(input_path);
+	np.save('abspace',abSpace)
+	pspace = (255.0*abSpace.astype(float)/np.max(abSpace)).astype(np.uint8);
+	print(abSpace)
+	plt.contourf(np.log(abSpace/numOfImg));
+	plt.show()
+
+if __name__ == '__main__':
+	main();
